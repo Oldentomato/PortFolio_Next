282e52f0a91da978d42361bab27c3e39acb8392b
Oldentomato <ender1412@naver.com>
2023-01-17 16:16:01 +0900

    게시글 작성

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,7 +8,7 @@ export default function Footer({ children }: AddLayoutProps) {
         <div>
           <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
             <Link href="/" className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-              <span className="ml-3 text-xl">PortFolio</span>
+              <span className="footerspan ml-3 text-xl">PortFolio</span>
             </Link>
             <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
               © 2020 Tailblocks
